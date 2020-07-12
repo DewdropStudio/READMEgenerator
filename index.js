64853285bb0e047db8b08010efde993c72728913
Rocio Avila <rocio.avila@northwestern.com>
2020-07-11 22:42:00 -0500

    Added questions for USER

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+const generateMarkdown = require("utils/generateMarkdown")
 // array of questions for user
 const questions = [
+    {
+        message: "What is your Github username?",
+        name: "username",
+        type: "input",
+    },
+    {
+        message: "What is your email address?",
+        name: "email",
+        type: "input",
+    },
+    {
+        message: "What is the Title of this project?",
+        name: "title",
+        type: "input",
+    },
+    {
+        message: "Provide a brief description.",
+        name: "description",
+        type: "input",
+    },
+    {
+        message: "Installation instructions for this project.",
+        name: "installation",
+        type: "input",
+    }
+    
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function to initialize program
-function init() {
+// // function to initialize program
+// function init() {
 
-}
+// }
 
-// function call to initialize program
-init();
+// // function call to initialize program
+// init();
