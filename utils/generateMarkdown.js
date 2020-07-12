995a8706b8f576c0bbfe22933ca084fc77355fa4
Rocio Avila <rocio.avila@northwestern.com>
2020-07-11 23:54:50 -0500

    README layout

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,9 +5,25 @@ function generateMarkdown(data) {
     # ${data.title}
 
     # Table of Contents
-    -
+    - [Description](#description)
+    - [Installation](#installation)
+    - [Questions](#username)
+    - [Contact](#email)
 
-  
+    ## Description:
+        ${data.description}
+
+    ## Installation:
+        ${data.installation}
+
+    ## Questions:
+        For questions, you can go to my GitHub page at the following Link:
+    
+        - [GitHub Profile](https://github.com/${data.username})
+
+    ## Contact
+       For additional questions please reach out to my email ${data.email}
+       
   `;
   }
   
